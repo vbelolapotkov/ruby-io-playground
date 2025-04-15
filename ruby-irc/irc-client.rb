817cd25169fff23d5de507248dbdcd56a373501e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2025-04-15 13:31:39 +0300

    Add basic error handling to prvent exceptions on closing

--- ruby-irc/irc-client.rb
+++ ruby-irc/irc-client.rb
@@ -5,25 +5,52 @@ client = TCPSocket.new "irc.freenode.net", 6667
 client.puts "user vb localhost irc.freenode.net :Playing with Ruby IRC"
 client.puts "nick vb"
 
+# Flag to control the main loop
+running = true
+
+# Handle Ctrl+C (SIGINT)
+Signal.trap("INT") do
+  puts "\nInterrupted externally. Shutting down gracefully..."
+  running = false
+  exit
+end
 
 # Print incoming messages
 Thread.start do
-  loop do
-    msg = client.gets
-    puts "Received: #{msg}"
-    if msg.include?("PING")
-      response = "PONG #{msg.split(" ")[1]}"
-      puts "Sending: #{response}"
-      client.puts "#{response}"
+  begin
+    loop do
+      msg = client.gets
+      break if msg.nil? # Connection closed
+      puts "Received: #{msg}"
+      if msg.include?("ERROR") 
+        puts "Exiting application"
+        running = false
+        client.close
+        exit
+      end
+      if msg.include?("PING")
+        response = "PONG #{msg.split(" ")[1]}"
+        puts "Sending: #{response}"
+        client.puts "#{response}"
+      end
     end
-  end  
+  rescue IOError => e
+    puts "Connection closed" if running # Only show message if we didn't initiate the close
+  end
 end
 
 # UI: send messages
-loop do
+while running
   msg = gets.chomp
-  client.puts "#{msg}"
+  if msg.downcase == "exit"
+    running = false
+    client.close
+    break
+  end
+  begin
+    client.puts "#{msg}"
+  rescue IOError => e
+    puts "Connection closed"
+    break
+  end
 end
-
-
-client.close
